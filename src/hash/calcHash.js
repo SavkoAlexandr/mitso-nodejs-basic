a2b6f6ace9132c182c18dc18bd5081932951bd46
Alexandr <166656261+SavkoAlexandr@users.noreply.github.com>
2026-03-30 20:14:14 +0300

    4 task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import crypto from "crypto";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    process.cwd(),
+    "files",
+    "fileToCalculateHashFor.txt",
+  );
+
+  try {
+    const content = await fs.readFile(filePath);
+    const hash = crypto.createHash("sha256").update(content).digest("hex");
+    console.log(hash);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await calculateHash();
