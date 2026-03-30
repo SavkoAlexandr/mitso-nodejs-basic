b04011ef581eea670000d03b5123a45f34b067d4
Alexandr <166656261+SavkoAlexandr@users.noreply.github.com>
2026-03-30 17:32:13 +0300

    2 task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+
+  const mitsoVars = [];
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith("MITSO_")) {
+      mitsoVars.push(`${key}= ${value}`);
+    }
+  }
+
+  console.log(mitsoVars.join(";"));
 };
 
 parseEnv();
