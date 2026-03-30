783fac70d25a650f978e1aa0813b86c7eb78c19f
Alexandr <166656261+SavkoAlexandr@users.noreply.github.com>
2026-03-30 19:14:53 +0300

    3 task

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,7 +1,10 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const random = Math.random();
 
@@ -17,8 +20,8 @@ console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
@@ -33,8 +36,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
-
