1e6ed75d33c6c07363b5391c6b0c7638d90f2e0c
Alexandr <166656261+SavkoAlexandr@users.noreply.github.com>
2026-04-06 23:34:55 +0300

    work threads done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const workers = [];
+  const results = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const worker = new Worker(path.join(__dirname, "worker.js"));
+    const startNumber = 10 + i;
+
+    workers.push(
+      new Promise((resolve) => {
+        worker.on("message", (data) => {
+          resolve({ status: "resolved", data });
+        });
+
+        worker.on("error", () => {
+          resolve({ status: "error", data: null });
+        });
+
+        worker.postMessage(startNumber);
+      }),
+    );
+  }
+
+  const allResults = await Promise.all(workers);
+  console.log(allResults);
 };
 
 await performCalculations();
